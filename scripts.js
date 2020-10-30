6bed03edd0587016c955d0b675db288b22dd7283
Kevin Johnson <kevin.johnson_12@hotmail.com>
2020-10-30 14:26:41 -0500

    Edited styles and scripts

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 /* Add your JavaScript to this file */
+
+window.onload = function(){
+
+
+    document.querySelector("button").addEventListener("click", function(){
+        var formData = document.getElementById('email').value;
+        let email = formData.email.value;
+        var message = document.querySelector(".message")
+        
+        if(email){
+            message.innerHTML = 'Thank you! Your email address <${email}> has been added to your mailing list';
+            return false;
+        }else{
+            message.innerHTML = 'Please enter a valid email';
+            return false;
+        }
+
+    })
+}
